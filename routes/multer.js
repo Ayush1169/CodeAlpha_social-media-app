052a43829b20b983f2fb8424d85f4797d90f17e6
Ayush Agrawal <2305774@kiit.ac.in>
2025-06-18 23:49:13 +0530

    Update code and styles

--- routes/multer.js
+++ routes/multer.js
@@ -5,7 +5,7 @@ const path = require("path");
 // Multer Storage Config
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads'); // make sure this folder exists!
+    cb(null, './public/uploads'); // make sure this folder exists!
   },
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname).toLowerCase();
